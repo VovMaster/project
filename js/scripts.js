7279cd998c71d4c422ddc56c1788413c04df259f
vladimir <antonsimeonvladimir@gmail.com>
2016-07-25 18:03:19 +0300

    update

--- js/scripts.js
+++ js/scripts.js
@@ -13,6 +13,119 @@ $(document).ready(function(){
 	});
     $( "#datepicker" ).datepicker({
 		showOtherMonths: true,
-		selectOtherMonths: true
+		selectOtherMonths: true,
     });
+
+
+
+    var dateSelect = [
+    	[
+	    	[
+	    		'dateNewAssignments'
+	    	],
+	    	[
+		    	{
+					'dateYear': '2016',
+					'dateMonth': '6',
+					'dateDay': '9'
+				}
+		    ]
+	    ],
+
+	    [
+	    	[
+	    		'dateBalances'
+	    	],
+	    	[
+		    	{
+					'dateYear': '2016',
+					'dateMonth': '6',
+					'dateDay': '11',
+				}
+		    ]
+	    ],
+	    [
+	    	[
+	    		'dateExercised'
+	    	],
+	    	[
+				{
+					'dateYear': '2016',
+					'dateMonth': '6',
+					'dateDay': '3',
+				},
+				{
+					'dateYear': '2016',
+					'dateMonth': '6',
+					'dateDay': '19',
+				},
+				{
+					'dateYear': '2016',
+					'dateMonth': '6',
+					'dateDay': '26',
+				}
+			]
+	    ],
+	    [
+	    	[
+	    		'datePendingApproval'
+	    	],
+	    	[
+		    	{
+					'dateYear': '2016',
+					'dateMonth': '6',
+					'dateDay': '24',
+				}
+		    ]
+	    ]
+    ];
+
+    // console.log(dateSelect);
+    // console.log(dateSelect.length);
+    // console.log(dateSelect.0);
+
+    		
+
+    function datePickerSelect(){
+
+    	$('#datepicker td').each(function(){
+    		var dayDatepic = $(this).children().text();
+    		$(this).attr('data-day', dayDatepic);
+    	});
+
+
+
+
+    	for (var i = 0; i < dateSelect.length; i++) {
+
+    		// console.log(dateSelect[i][0][0]);
+    		for (var b = 0; b < dateSelect[i][1].length; b++) {
+
+    			$('#datepicker td[data-day="' + dateSelect[i][1][b].dateDay + '"][data-month="' + dateSelect[i][1][b].dateMonth + '"][data-year="' + dateSelect[i][1][b].dateYear + '"]').addClass(dateSelect[i][0][0]);
+    			// $('#datepicker td[data-day="' + dateSelect[i][1][b].dateDay + '"]').addClass(dateSelect[i][0][0]);
+    			// console.log(dateSelect[i][1][b].dateDay);
+    			// console.log(dateSelect[i][1][b].dateMonth);
+    			// console.log(dateSelect[i][1][b].dateYear);
+
+    		}
+
+
+    	}
+
+    }
+
+    datePickerSelect();
+
+
+
+	$(document).on('click', '.ui-datepicker-next', function () {
+	   datePickerSelect();  
+	})
+
+	$(document).on('click', '.ui-datepicker-prev', function () {
+	   datePickerSelect();
+	})
+
+
+
 });
